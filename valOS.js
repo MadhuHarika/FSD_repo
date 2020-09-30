96bef865bc23b19eff04060e96afb2468c5430af
MadhuNandipati <nandipati.madhuharika34@gmail.com>
2020-09-30 17:00:04 +0530

    check the OS related info

--- valOS.js
+++ valOS.js
@@ -5,4 +5,9 @@ let arc=os1.arch();
 let hostName=os1.hostname();
 let freeMem=os1.freemem();
 let nwInterface=os1.networkInterfaces();
+let type1=os1.type();
+let upTime=os1.uptime();
+let ttlmem=os1.totalmem();
+
 console.log(`${rel} ${plat} ${arc} ${hostName} ${freeMem} ${nwInterface}`);
+console.log(`${type1} ${upTime} ${ttlmem}`);
