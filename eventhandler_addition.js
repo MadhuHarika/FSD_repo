2feff3a43bbede729bb7bbbac0e32f9c8b6ba4fc
MadhuNandipati <nandipati.madhuharika34@gmail.com>
2020-09-30 18:03:54 +0530

    event handler test for addition,objects by passing parameter value using lamda function

--- eventhandler_addition.js
+++ eventhandler_addition.js
@@ -6,5 +6,12 @@ emitter.on('addition of two numbers',(fnum,snum)=>
     let result=fnum+snum;
     console.log(result);
 });
+emitter.on('message to',(arg1,arg2)=>
+{
+    console.log(arg1);
+    console.log(arg2);
+})
 
 emitter.emit('addition of two numbers',200,200);
+emitter.emit('message to',{id:1,name:'Madhu Harika'},{id:2, name:'Kanishk'});
+emitter.emit('message to',{name:'welcome'},{name: 'to Kanishk'});
