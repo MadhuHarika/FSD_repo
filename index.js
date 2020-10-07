373d4b7a5fe7d8d5f8215a732cf497d6aa6bcb81
MadhuNandipati <nandipati.madhuharika34@gmail.com>
2020-10-07 14:51:47 +0530

    adding external css

---
+++ index.js
@@ -0,0 +1,16 @@
+const path=require('path');
+const express=require('express');
+const bodyParser=require('body-parser');
+const app=express();
+//to post any static content
+app.use(express.static('public'));
+const adminRoutes = require('./routes/admin');
+const shopRoutes=require('./routes/shop');
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+app.use((req,res,next)=>{
+    res.status(404).sendFile(path.join(__dirname,'views','errorPage.html'));
+});
+app.listen(3000);
