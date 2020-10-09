4de7e1a5127ef68a4c0a04a26b051dc8c53246f8
MadhuNandipati <nandipati.madhuharika34@gmail.com>
2020-10-09 12:01:46 +0530

    courses-updatamany - update single data

---
+++ index.js
@@ -0,0 +1,66 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/play-ground', {useNewUrlParser: true, useUnifiedTopology: true});
+
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [ String ],
+    date: Date,
+    isPublished: Boolean,
+    price: Number
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+//find the course
+
+// const course = new Course({
+//     name:'Spring',
+//     author: 'Jane',
+//     tags:['Spring','Middleware'],
+//     isPublished: true,
+//     price:34
+// });
+//
+// async function createCourse()
+// {
+//     const result=await course.save();
+//     console.log('Course created');
+// }
+
+//used to update the data by fetching the id.
+
+/*async function updateCourse(id)
+{
+    const course=await Course.findById(id);
+    console.log(course);
+    if(!course) return;
+    course.name='Spring';
+    course.author='Jane';
+    course.save();
+    console.log(course);
+}
+updateCourse('5f7ff23386ef4d0d48d068fc').then(()=>{
+    console.log('Course update success');
+});*/
+// updating lot of data at a time
+
+// async function updateCourse()
+// {
+// const result=await Course.updateMany({author:'Mosh'},
+//     {$set:{name:'Learning programming'}});
+// console.log('course update success');
+// }
+
+//update for a single data
+
+async function updateCourse()
+{
+    const result=await Course.update({author:'Mosh'},{$set:{name:'Learning cricket'}});
+    console.log('course update success');
+}
+
+updateCourse().then(()=>{
+    console.log('success');
+})
