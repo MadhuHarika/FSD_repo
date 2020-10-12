f036158af4a5999d9a2ac7ea7c3f0a9f8bf695cf
MadhuNandipati <nandipati.madhuharika34@gmail.com>
2020-10-12 10:01:17 +0530

    express-mongo-connection-post-get-put-delete

--- routes/routes-mongo.js
+++ routes/routes-mongo.js
@@ -8,18 +8,61 @@ const genreSchema = {
 }
 const Genre = mongoose.model('genre', genreSchema);
 
-router.post('/',async(req,res)=>{
-let result = await Genre.find();
-res.send(result);
-})
+// router.post('/',async(req,res)=>{
+// let result = await Genre.find();
+// res.send(result);
+//})
 
 router.post('/', async (req, res) => {
-    const { error } = validateGenre(req.body);
+    const {error} = validateGenre(req.body);
     if (error) return res.status(400).send(error.details[0].message);
-    let genre = new Genre({ name: req.body.name });
+    let genre = new Genre({name: req.body.name});
     let result = await genre.save();
     res.send(result);
 });
+
+
+router.get('/', async (req, res) => {
+    let result = await Genre.find();
+    res.send(result);
+})
+
+//update by id
+// router.put('/:id', async (req, res) => {
+//     const {error} = validateGenre(req.body);
+//     if (error) {
+//         return res.status(400).send(error.details[0].message);
+//     }
+//     const genre = await Genre.findByIdAndUpdate(req.params.id, {name: req.body.name},{new:true});
+//     if (!genre) {
+//         return res.status(404).send('The genre with the given ID was not found.');
+//     }
+//     res.send(genre);
+// });
+
+//update first
+
+router.put('/:id', async (req, res) => {
+    // const {error} = validateGenre(req.body);
+    // if (error) {
+    //     return res.status(400).send(error.details[0].message);
+    // }
+    const genre = await Genre.findOneAndUpdate({_id: req.params.id}, {$set: {name: req.body.name}}, {new: true});
+    // if (!genre) {
+    //     return res.status(404).send('The genre with the given ID was not found.');
+    // }
+    res.send(genre);
+})
+//delete the record
+
+router.delete('/:id', async (req, res) => {
+    const genre = await Genre.findByIdAndRemove({_id: req.params.id});
+    if (!genre) {
+            return res.status(404).send('The genre with the given ID was not found.');
+        }
+    res.send(genre);
+})
+
 function validateGenre(genre) {
     const schema = {
         name: Joi.string().min(5).required()
