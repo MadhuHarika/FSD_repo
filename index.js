764374bd5dd89b5f2fe1aa5018c2b5ce47c40887
MadhuNandipati <nandipati.madhuharika34@gmail.com>
2020-10-13 11:49:50 +0530

    implementing pug with conditions

---
+++ index.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');;
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'pug');
+app.set('views','views');
+app.use('/admin', adminData.router);
+app.use(shopRoutes);
+app.use((req, res, next) => {
+    res.render('errorpage',{pageTitle: 'error'});
+});
+
+app.listen(3000);
